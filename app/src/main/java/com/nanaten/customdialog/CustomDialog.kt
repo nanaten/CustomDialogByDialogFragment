74697f23969a3fbe1aed17a3a05f2cf4f2c2d601
m.coder <kurokoshow00@gmail.com>
2021-02-23 16:59:48 +0900

    Add: Builderパターンでダイアログ表示処理を実装

--- app/src/main/java/com/nanaten/customdialog/CustomDialog.kt
+++ app/src/main/java/com/nanaten/customdialog/CustomDialog.kt
@@ -6,17 +6,61 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.nanaten.customdialog.databinding.LayoutCustomDialogBinding
+import java.io.Serializable
 
 class CustomDialog() : DialogFragment() {
     private var title: String = ""
     private var message: String = ""
+    private var positiveButtonText: String = ""
+    private var negativeButtonText: String = ""
+    private var positiveButtonClickListener: ButtonClickListener? = null
+    private var negativeButtonClickListener: ButtonClickListener? = null
 
     companion object {
-        fun create(title: String, message: String): CustomDialog {
-            return CustomDialog().apply {
-                val bundle = Bundle()
+        private const val DEFAULT_POSITIVE_BUTTON_TEXT = "OK"
+        private const val DEFAULT_NEGATIVE_BUTTON_TEXT = "キャンセル"
+    }
+
+    class Builder() {
+        private val bundle = Bundle()
+        fun setTitle(title: String): Builder {
+            return this.apply {
                 bundle.putString("TitleKey", title)
+            }
+        }
+
+        fun setMessage(message: String): Builder {
+            return this.apply {
                 bundle.putString("MessageKey", message)
+            }
+        }
+
+        fun setPositiveButton(buttonText: String, listener: () -> Unit): Builder {
+            return this.apply {
+                bundle.putString("PositiveButtonTextKey", buttonText)
+                val positiveButtonListener = object : ButtonClickListener {
+                    override fun onClick() {
+                        listener.invoke()
+                    }
+                }
+                bundle.putSerializable("PositiveButtonListenerKey", positiveButtonListener)
+            }
+        }
+
+        fun setNegativeButton(buttonText: String, listener: () -> Unit): Builder {
+            return this.apply {
+                bundle.putString("NegativeButtonTextKey", buttonText)
+                val negativeButtonListener = object : ButtonClickListener {
+                    override fun onClick() {
+                        listener.invoke()
+                    }
+                }
+                bundle.putSerializable("NegativeButtonListenerKey", negativeButtonListener)
+            }
+        }
+
+        fun build(): CustomDialog {
+            return CustomDialog().apply {
                 arguments = bundle
             }
         }
@@ -30,13 +74,32 @@ class CustomDialog() : DialogFragment() {
         arguments?.let {
             title = it.getString("TitleKey", "")
             message = it.getString("MessageKey", "")
+            positiveButtonText = it.getString("PositiveButtonTextKey", DEFAULT_POSITIVE_BUTTON_TEXT)
+            negativeButtonText = it.getString("NegativeButtonTextKey", DEFAULT_NEGATIVE_BUTTON_TEXT)
+            positiveButtonClickListener =
+                it.getSerializable("PositiveButtonListenerKey") as ButtonClickListener
+            negativeButtonClickListener =
+                it.getSerializable("NegativeButtonListenerKey") as ButtonClickListener
         }
         val binding = LayoutCustomDialogBinding.inflate(requireActivity().layoutInflater)
         binding.title.text = title
         binding.message.text = message
-        binding.positiveButton.text = "OK"
-        binding.negativeButton.text = "キャンセル"
+        binding.positiveButton.text = positiveButtonText
+        binding.negativeButton.text = negativeButtonText
+        binding.positiveButton.setOnClickListener {
+            dismiss()
+            positiveButtonClickListener?.onClick()
+        }
+        binding.negativeButton.setOnClickListener {
+            dismiss()
+            negativeButtonClickListener?.onClick()
+        }
         dialog.setContentView(binding.root)
         return dialog
     }
+}
+
+
+interface ButtonClickListener : Serializable {
+    fun onClick()
 }
--- app/src/main/java/com/nanaten/customdialog/MainActivity.kt
+++ app/src/main/java/com/nanaten/customdialog/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nanaten.customdialog
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.nanaten.customdialog.databinding.ActivityMainBinding
 
@@ -9,11 +10,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
+
         binding.dialogButton.setOnClickListener {
-            CustomDialog
-                .create("カスタムタイトル", "カスタムメッセージ")
+            CustomDialog.Builder()
+                .setTitle("タイトル")
+                .setMessage("カスタムメッセージ")
+                .setPositiveButton("はい") { showToast("はいが押されました") }
+                .setNegativeButton("いいえ") { showToast("いいえが押されました") }
+                .build()
                 .show(supportFragmentManager, CustomDialog::class.simpleName)
         }
         setContentView(binding.root)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
