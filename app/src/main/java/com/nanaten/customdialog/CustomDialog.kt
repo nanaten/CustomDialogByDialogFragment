cbe09afc49389b92df1a7f72a17c43c11fbc5d3d
m.coder <kurokoshow00@gmail.com>
2021-04-03 00:38:56 +0900

    Fix: CustomDialogのリスナーの処理修正

--- app/src/main/java/com/nanaten/customdialog/CustomDialog.kt
+++ app/src/main/java/com/nanaten/customdialog/CustomDialog.kt
@@ -4,31 +4,32 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import com.nanaten.customdialog.databinding.LayoutCustomDialogBinding
-import java.io.Serializable
 
-class CustomDialog() : DialogFragment() {
+class CustomDialog : DialogFragment() {
     private var title: String = ""
     private var message: String = ""
     private var positiveButtonText: String = ""
     private var negativeButtonText: String = ""
-    private var positiveButtonClickListener: ButtonClickListener? = null
-    private var negativeButtonClickListener: ButtonClickListener? = null
 
     companion object {
         private const val DEFAULT_POSITIVE_BUTTON_TEXT = "OK"
         private const val DEFAULT_NEGATIVE_BUTTON_TEXT = "キャンセル"
         private const val TITLE_KEY = "TitleKey"
         private const val MESSAGE_KEY = "MessageKey"
+        private const val REQUEST_POSITIVE_BUTTON_KEY = "RequestPositiveButtonKey"
+        private const val REQUEST_NEGATIVE_BUTTON_KEY = "RequestNegativeButtonKey"
         private const val POSITIVE_BUTTON_TEXT_KEY = "PositiveButtonTextKey"
-        private const val POSITIVE_BUTTON_LISTENER_KEY = "PositiveButtonListenerKey"
         private const val NEGATIVE_BUTTON_TEXT_KEY = "NegativeButtonTextKey"
-        private const val NEGATIVE_BUTTON_LISTENER_KEY = "NegativeButtonListenerKey"
     }
 
-    class Builder() {
+    class Builder(private val fragment: Fragment) {
         private val bundle = Bundle()
+
         fun setTitle(title: String): Builder {
             return this.apply {
                 bundle.putString(TITLE_KEY, title)
@@ -44,24 +45,28 @@ class CustomDialog() : DialogFragment() {
         fun setPositiveButton(buttonText: String, listener: (() -> Unit)? = null): Builder {
             return this.apply {
                 bundle.putString(POSITIVE_BUTTON_TEXT_KEY, buttonText)
-                val positiveButtonListener = object : ButtonClickListener {
-                    override fun onClick() {
-                        listener?.invoke()
-                    }
+                fragment.apply {
+                    this.childFragmentManager
+                        .setFragmentResultListener(
+                            REQUEST_POSITIVE_BUTTON_KEY,
+                            this.viewLifecycleOwner
+                        ) { _, _ ->
+                            listener?.invoke()
+                        }
                 }
-                bundle.putSerializable(POSITIVE_BUTTON_LISTENER_KEY, positiveButtonListener)
             }
         }
 
         fun setNegativeButton(buttonText: String, listener: (() -> Unit)? = null): Builder {
             return this.apply {
                 bundle.putString(NEGATIVE_BUTTON_TEXT_KEY, buttonText)
-                val negativeButtonListener = object : ButtonClickListener {
-                    override fun onClick() {
+                fragment.childFragmentManager
+                    .setFragmentResultListener(
+                        REQUEST_NEGATIVE_BUTTON_KEY,
+                        fragment.viewLifecycleOwner
+                    ) { _, _ ->
                         listener?.invoke()
                     }
-                }
-                bundle.putSerializable(NEGATIVE_BUTTON_LISTENER_KEY, negativeButtonListener)
             }
         }
 
@@ -84,10 +89,6 @@ class CustomDialog() : DialogFragment() {
                 it.getString(POSITIVE_BUTTON_TEXT_KEY, DEFAULT_POSITIVE_BUTTON_TEXT)
             negativeButtonText =
                 it.getString(NEGATIVE_BUTTON_TEXT_KEY, DEFAULT_NEGATIVE_BUTTON_TEXT)
-            positiveButtonClickListener =
-                it.getSerializable(POSITIVE_BUTTON_LISTENER_KEY) as? ButtonClickListener
-            negativeButtonClickListener =
-                it.getSerializable(NEGATIVE_BUTTON_LISTENER_KEY) as? ButtonClickListener
         }
         val binding = LayoutCustomDialogBinding.inflate(requireActivity().layoutInflater)
         binding.title.text = title
@@ -96,18 +97,19 @@ class CustomDialog() : DialogFragment() {
         binding.negativeButton.text = negativeButtonText
         binding.positiveButton.setOnClickListener {
             dismiss()
-            positiveButtonClickListener?.onClick()
+            setFragmentResult(
+                REQUEST_POSITIVE_BUTTON_KEY,
+                bundleOf()
+            )
         }
         binding.negativeButton.setOnClickListener {
             dismiss()
-            negativeButtonClickListener?.onClick()
+            setFragmentResult(
+                REQUEST_NEGATIVE_BUTTON_KEY,
+                bundleOf()
+            )
         }
         dialog.setContentView(binding.root)
         return dialog
     }
-}
-
-
-interface ButtonClickListener : Serializable {
-    fun onClick()
 }
